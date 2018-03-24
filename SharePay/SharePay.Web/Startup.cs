579d68e961b23434cce156c5943a7ba2a87232df
Krasimir Vasilev <k.vasilev9403@gmail.com>
2018-03-24 15:37:36 +0200

    Solution Setup

---
+++ SharePay/SharePay.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SharePay.Web.Startup))]
+namespace SharePay.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
